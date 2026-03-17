edbcdc44b816e1fc7abe25340f2944982c4fd33d
Eyerusalem-Rufael <jerryrufael32@gmail.com>
2026-03-17 09:48:56 +0300

    Add solution for Minimum Replacements to Sort the Array

--- leetcode/2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.py
+++ leetcode/2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.py
@@ -1,16 +1,16 @@
 class Solution:
     def minimumReplacement(self, nums: List[int]) -> int:
         n = len(nums) 
-        prev = nums[-1] 
+        limit = nums[-1] 
         count = 0
 
         for i in range(n-2,-1,-1):
-            if nums[i] > prev:
-                partitions= ceil(nums[i] / prev)
+            if nums[i] > limit:
+                partitions = ceil(nums[i] / limit)
                 count += partitions- 1
-                prev = nums[i] // partitions
+                limit = nums[i] // partitions
             else:
-                prev = nums[i]
+                limit = nums[i]
 
         return count
 
