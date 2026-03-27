86c4a921ac809830efe9cc0aa71af512d492c070
Eyerusalem-Rufael <jerryrufael32@gmail.com>
2026-03-27 10:12:57 +0300

    Add solution for Two Sum IV - Input is a BST

---
+++ leetcode/0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        value = set()
+        def inorder(node):
+            if not node:
+                return False
+
+            if inorder(node.left): #left
+                return True
+
+            if k - node.val in value:
+                return True
+            value.add(node.val) #parent
+
+            if inorder(node.right):#right
+                return True
+            
+            return False
+
+        return inorder(root)
