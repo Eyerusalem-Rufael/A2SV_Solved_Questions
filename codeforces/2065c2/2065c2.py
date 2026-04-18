4290877d19753b238205da38997fc6c92a3278b6
Eyerusalem-Rufael <jerryrufael32@gmail.com>
2026-04-18 07:31:30 +0300

    Add solution for 2065c2

--- codeforces/2065c2/2065c2.py
+++ codeforces/2065c2/2065c2.py
@@ -1,32 +1,32 @@
 from bisect import bisect_left
- 
+
 t = int(input())
 for _ in range(t):
     n, m = map(int, input().split())
     a = list(map(int, input().split()))
     b = list(map(int, input().split()))
     b.sort()
- 
+
     flag = True
     prev = float('-inf')
     for i in range(n):
         curr = a[i]
         opt= float('inf')
- 
+
         #opt-1
         if curr >= prev:
             opt = curr
- 
+
         #opt-2
         idx = bisect_left(b,curr + prev)
         
         if idx < m:
             opt = min(opt,b[idx] - a[i])
- 
+
         if opt == float('inf'):
             flag = False
             break
- 
+
         prev = opt
- 
+
     print('YES' if flag else 'NO')
